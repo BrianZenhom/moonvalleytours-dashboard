de681f0c844b36a458710eb285a54ca201b867a7
BrianZenhom <contact@brianzenhom.dev>
2023-11-23 00:28:52 +0100

    Add styles and functionality to the countries page

--- app/dashboard/countries/page.jsx
+++ app/dashboard/countries/page.jsx
@@ -1,5 +1,64 @@
+import styles from '@/app/ui/dashboard/countries/countries.module.css';
+import Pagination from '@/app/ui/dashboard/pagination/pagination';
+import Search from '@/app/ui/dashboard/search/search';
+import Image from 'next/image';
+import Link from 'next/link';
+
 const CountriesPage = () => {
-  return <div>Countries</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a user..." />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Header image</td>
+            <td>Country</td>
+            <td>Continent</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt=""
+                  width="40"
+                  height="40"
+                  className={styles.userImage}
+                />
+                Susana Abdel Salam
+              </div>
+            </td>
+            <td>susana@moonvalleytours.com</td>
+            <td>Admin</td>
+            <td>22 april 2024</td>
+            <td>Active</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.delete}`}>
+                    Delete
+                  </button>
+                </Link>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
 };
 
 export default CountriesPage;
